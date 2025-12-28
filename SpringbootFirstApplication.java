5fb81e5db74c0d371e11eb825cbb2e460bab5f21
Avdhi Tembhare <83524540+Avdhi16@users.noreply.github.com>
2025-12-28 22:14:46 +0530

    Add files via upload

---
+++ SpringbootFirstApplication.java
@@ -0,0 +1,13 @@
+package springboot.quickstart.springbootFirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootFirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootFirstApplication.class, args);
+	}
+
+}
